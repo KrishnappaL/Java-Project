68f82b09c19df4a3acb8488f99b115cbf74510e2
Lakshmi <142583809+KrishnappaL@users.noreply.github.com>
2024-07-09 19:19:26 +0530

    Add files via upload

---
+++ Passenger.java
@@ -0,0 +1,17 @@
+package codingEx1;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Passenger {
+    String name;
+    int age;
+    String gender;
+    String berthPreference;
+
+    public Passenger(String name, int age, String gender, String berthPreference) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.berthPreference = berthPreference;
+    }
+}
